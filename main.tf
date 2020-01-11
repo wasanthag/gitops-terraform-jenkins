dec71434e9297690e0ddbab00f0f0f965d50b33c
Alex Podobnik <alexandar.podobnik@gmail.com>
2020-01-11 09:13:25 -0500

    Adding Terraform 0.12 Support

--- main.tf
+++ main.tf
@@ -14,14 +14,14 @@ provider "aws" {
 
 # Create EC2 instance
 resource "aws_instance" "default" {
-  ami                    = "${var.ami}"
-  count                  = "${var.count}"
-  key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  ami                    = var.ami
+  count                  = var.instance_count
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
-  instance_type          = "${var.instance_type}"
+  instance_type          = var.instance_type
 
-  tags {
+  tags = {
     Name = "terraform-default"
   }
 }
